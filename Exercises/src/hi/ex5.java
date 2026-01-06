d88a9579ad6133ff4b2bfac945305d5d9a1c0e5d
Namuunbaigali Barsbaatar <luuvsuzy@gmail.com>
2026-01-06 16:52:52 +0900

    added ex5

---
+++ Exercises/src/hi/ex5.java
@@ -0,0 +1,11 @@
+package hi;
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double N = sc.nextDouble();
+        System.out.printf("%.4f%n", N);
+        sc.close();
+    }
+}
